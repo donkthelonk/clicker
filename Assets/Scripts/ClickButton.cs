d15d8866aef174d91d5bf31806a048422cd4e3bc
Robert Giles <robert.ryan.giles@gmail.com>
2018-06-19 17:06:54 -0500

    Add existing project files to respository

---
+++ Assets/Scripts/ClickButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickButton : MonoBehaviour {
+
+	public Button clickButton;
+	public Text currentValue;
+
+	public int value;
+
+	// Use this for initialization
+	void Start () {
+		value = 0;
+		currentValue.text = value.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnClick() {
+		value++;
+		currentValue.text = value.ToString();
+	}
+}
